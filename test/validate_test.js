719ce7f8673f1ddc9717bbf3e584a3f882545eca
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-06-16 15:47:21 -0700

    Fixed validation_tests

--- test/validate_test.js
+++ test/validate_test.js
@@ -1,55 +1,25 @@
-var validate    = require('../utils/validate');
-var fs          = require('fs');
+var base        = require('taskcluster-base');
 var path        = require('path');
 
-/**
- * Test cases each defined with relative path, schema identifier and success as
- * either true or false depending on whether or not validation should succeed
- *
- * Add your schema tests below...
- */
-var testCases = [
-  {
-    path:     'schemas/task-graph-example.json',
-    schema:   'http://schemas.taskcluster.net/scheduler/v1/task-graph.json#',
-    success:  true,
-  }, {
-    path:     'schemas/invalid-task-graph-example.json',
-    schema:   'http://schemas.taskcluster.net/scheduler/v1/task-graph.json#',
-    success:  false,
-  }
-];
-
-// define a test for each test file
-testCases.forEach(function(testCase) {
-  exports[testCase.path] = function(test) {
-    test.expect(1);
-
-    // Setup validate if needed, before we continue
-    validate.setup().then(function() {
-      // Load test data
-      var filePath = path.join(__dirname, testCase.path);
-      var data = fs.readFileSync(filePath, {encoding: 'utf-8'});
-      var json = JSON.parse(data);
-
-      // Validate json
-      var errors = validate(json, testCase.schema);
-
-      // Test errors
-      if(testCase.success) {
-        test.ok(errors === null, "Schema doesn't match test for " + testCase.path);
-        if (errors !== null) {
-          console.log("Errors:");
-          errors.forEach(function(error) {
-            console.log(error);
-          });
-        }
-      } else {
-        test.ok(errors !== null, "Schema matches unexpectedly test for " + testCase.path);
+suite('validate', function() {
+  // Run test cases using schemas testing utility from taskcluster-base
+  base.testing.schemas({
+    validator: {
+      folder:       path.join(__dirname, '..', 'schemas'),
+      constants:    require('../schemas/constants')
+    },
+    basePath:       path.join(__dirname, 'validate_test'),
+    schemaPrefix:   'http://schemas.taskcluster.net/',
+    cases: [
+      {
+        schema:   'scheduler/v1/task-graph.json#',
+        path:     'task-graph-example.json',
+        success:  true,
+      }, {
+        schema:   'scheduler/v1/task-graph.json#',
+        path:     'invalid-task-graph-example.json',
+        success:  false,
       }
-
-      // okay tests are done
-      test.done();
-    });
-  };
+    ]
+  });
 });
